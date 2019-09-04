4003ddcd48a5ce7fcb6c5232c4bc7645038e1637
chaowang19 <chaowang19@iflytek.com>
2019-09-04 16:57:03 +0800

    初始化项目文件

---
+++ app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '')));
+app.listen('8888', 'localhost', () => {
+
+})
